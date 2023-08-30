5c401828ab698b400f09f0b7eeea60277ca5b177
liumin16 <liumin16@corp.netease.com>
2023-08-30 14:45:28 +0800

    fix(formily): move designers to antd

--- packages/formily/src/designers/form-item.tsx
+++
@@ -1,6 +0,0 @@
-import { withDnd } from '@music163/tango-helpers';
-import { FormItem as BaseFormItem } from '../form-item';
-
-export const FormItem = withDnd({
-  name: 'FormItem',
-})(BaseFormItem);
--- packages/formily/src/designers/form.tsx
+++
@@ -1,6 +0,0 @@
-import { withDnd } from '@music163/tango-helpers';
-import { Form as BaseForm } from '../form';
-
-export const Form = withDnd({
-  name: 'Form',
-})(BaseForm);

05bb2e4bcea9a801d6c8e4ec181b455b02a3802b
wwsun <ww.sww@outlook.com>
2024-03-22 19:54:00 +0800

    feat: add mail package

---
+++ packages/mail/README.md
@@ -0,0 +1,11 @@
+# `@music163/tango-mail`
+
+> TODO: description
+
+## Usage
+
+```
+const tangoMail = require('@music163/tango-mail');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/mail/src/components/button.ts
@@ -0,0 +1,6 @@
+import { defineComponent } from '@music163/tango-boot';
+import { Button as EButton } from '@react-email/components';
+
+export const Button = defineComponent(EButton, {
+  name: 'Button',
+});

94921f9a6eef1c952edb3034b646f34d3af4c4ee
Wells <ww.sun@outlook.com>
2024-04-25 20:28:36 +0800

    fix: update components (#5)
    * fix: update prototypes
    
    * chore: fix deps
    
    * docs: update
    
    ---------
    
    Co-authored-by: wwsun <ww.sww@outlook.com>

--- packages/docs/stories/Page.stories.js
+++ packages/docs/stories/Page.stories.js
@@ -1,4 +1,4 @@
-import { Page, Section } from '@music163/antd';
+import { Column, Columns, Page, Section } from '@music163/antd';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -23,4 +23,37 @@ export const Basic = () => {
 
 export const Empty = {
   args: {},
+};
+
+export const Columns2 = () => {
+  return (
+    <Page title="2 columns">
+      <Columns columnGap="12px">
+        <Column bg="#FFF" colSpan={6} p={12}>
+          1/2
+        </Column>
+        <Column bg="#FFF" colSpan={6} p={12}>
+          1/2
+        </Column>
+      </Columns>
+    </Page>
+  );
+};
+
+export const Columns3 = () => {
+  return (
+    <Page title="3 columns">
+      <Columns columnGap="12px">
+        <Column bg="#FFF" colSpan={4} p={12}>
+          1/3
+        </Column>
+        <Column bg="#FFF" colSpan={4} p={12}>
+          1/3
+        </Column>
+        <Column bg="#FFF" colSpan={4} p={12}>
+          1/3
+        </Column>
+      </Columns>
+    </Page>
+  );
 };

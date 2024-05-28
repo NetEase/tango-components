f2c152bce5b187f3279ea7933c7a59e691918be1
wwsun <ww.sww@outlook.com>
2024-05-28 14:15:06 +0800

    chore: add mode switch

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,8 +1,5 @@
+import { useEffect } from 'react';
 import '@music163/antd/lib/esm/index.less';
-
-window.__TANGO_DESIGNER__ = {
-  version: '1.0.0',
-};
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -16,5 +13,39 @@ const preview = {
     },
   },
 };
+
+export const globalTypes = {
+  renderMode: {
+    name: 'Render Mode',
+    description: '渲染模式',
+    defaultValue: 'Normal',
+    toolbar: {
+      icon: 'paintbrush',
+      title: 'Mode',
+      items: ['Normal', 'Design'],
+      dynamicTitle: true,
+    },
+  },
+};
+
+export const decorators = [
+  (Story, context) => {
+    const mode = context.globals.renderMode;
+    useEffect(() => {
+      if (mode === 'Design') {
+        window.__TANGO_DESIGNER__ = {
+          version: '1.0.0',
+        };
+      } else {
+        delete window.__TANGO_DESIGNER__;
+      }
+      return () => {
+        delete window.__TANGO_DESIGNER__;
+      };
+    }, [mode]);
+
+    return <Story />;
+  },
+];
 
 export default preview;
